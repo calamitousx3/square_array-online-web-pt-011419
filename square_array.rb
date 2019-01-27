99c4e963516c99d9a666af1e5d198b1442a134d3
Carmen Renee <carmengenpt@gmail.com>
2019-01-27 17:07:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,19 @@
 array = [1,2,3]
 
 def square_array(array)
+<<<<<<< HEAD
   array2 = []
   
   array.each do |squared|
     array2 << (squared ** 2)
 end
 array2
+=======
+  
+  array.each do |squared|
+    puts math.sqrt#{squared}
+end
+>>>>>>> 6edb223d255309b0c9a00f690d710fa93c2dd528
 end
 
 
@@ -14,4 +21,8 @@ end
 # it 'should square the elements in an array' do
 #    expect(square_array([1,2,3])).to eq([1,4,9])
 #    expect(square_array([9,10,16,25])).to eq([81,100,256,625])
+<<<<<<< HEAD
 # x, Math.sqrt(x)
+=======
+  
+>>>>>>> 6edb223d255309b0c9a00f690d710fa93c2dd528
